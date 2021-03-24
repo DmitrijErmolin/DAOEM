9f865fe3b3ade31cd3b10543a259bea43e713dda
dmitr <dmitrij.ermolin.2013@mail.ru>
2021-03-24 16:50:26 +0300

    fix rating

--- rating.py
+++ rating.py
@@ -5,14 +5,14 @@ class RatingBase:
     def __init__(self, name):
         self.name = name
         self.parameters = dict()
-        self.parameters['default'] = 0.0
-        self.user_rating = 0.0
+        self.parameters['default'] = 0
+        self.user_rating = 0
         self.parameters['rang'] = 'Common user'
         self.parameters['precision_per_active'] = 0.001
         self.parameters['precision_per_generate'] = 0.5
         self.parameters['precision_per_git'] = 1
         self.parameters['superuser'] = False
-        self.parameters['up_limit_to_super_user'] = 5.0
+        self.parameters['up_limit_to_super_user'] = 5
         self.parameters['up_limit_to_all_user'] = 10
         self.parameters['down_limit_to_all_user'] = 0
 
@@ -32,7 +32,6 @@ class RatingBase:
             self.check_limits()
 
     def update_user_rating_per_active(self, time_delta, down_rate=False):
-        time_delta = round(time_delta, 1)
         if not down_rate:
             self.user_rating = self.user_rating + self.parameters['precision_per_active'] * time_delta
             self.check_limits()
@@ -43,11 +42,11 @@ class RatingBase:
 
     def update_user_rating_per_git(self, down_rate=False):
         if not down_rate:
-            self.user_rating += self.parameters['precision_per_generate']
+            self.user_rating += self.parameters['precision_per_git']
             self.check_superuser()
             self.check_limits()
         else:
-            self.user_rating -= self.parameters['precision_per_generate']
+            self.user_rating -= self.parameters['precision_per_git']
             self.check_superuser()
             self.check_limits()
 
@@ -64,8 +63,7 @@ class RatingBase:
             self.user_rating = self.parameters['up_limit_to_all_user']
         if self.user_rating < self.parameters['down_limit_to_all_user']:
             self.user_rating = self.parameters['down_limit_to_all_user']
-        round(self.user_rating, 3)
-        #todo fix float
+        self.user_rating = round(self.user_rating, 3)
 
     def become_superuser(self):
         self.parameters['superuser'] = True
@@ -74,28 +72,23 @@ class RatingBase:
         self.parameters['superuser'] = False
 
 
-
-
 if __name__ == '__main__':
-    pass
-    # start = time.time()
-    # time.sleep(1)
-    # time_d = time.time() - start
-    # test = RatingBase("12")
-    # print(test)
-    # test.update_user_rating_per_active(time_d)
-    # print(test)
-    # print()
-    # for i in range(20):
-    #     test.update_user_rating_per_gen()
-    #     print(test)
-    # test.become_superuser()
-    # for i in range(20):
-    #     test.update_user_rating_per_gen()
-    #     print(test)
-    # for i in range(25):
-    #     test.update_user_rating_per_gen(down_rate=True)
-    #     print(test)
+
+    test = RatingBase("12")
+    print(test)
+    print()
+    for i in range(20):
+        test.update_user_rating_per_gen()
+        print(test)
+    for i in range(25):
+        test.update_user_rating_per_gen(down_rate=True)
+        print(test)
+    for i in range(25):
+        test.update_user_rating_per_git()
+        print(test)
+    for i in range(40):
+        test.update_user_rating_per_gen(down_rate=True)
+        print(test)
 
 
 
